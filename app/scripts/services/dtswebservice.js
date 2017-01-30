d5c4ad08cd9211c8e1cc00c7fdf7453b5183d43e
Eddie Maas <eddie@edease.nl>
2017-01-30 08:55:00 +0100

    local dev server

--- app/scripts/services/dtswebservice.js
+++ app/scripts/services/dtswebservice.js
@@ -19,7 +19,7 @@ angular.module('dtsAdminApp')
       return $http({
         method: 'GET',
         //TODO TODO TODO  switch onderstaande url: './api/api2.php' --  ' http://localhost:80/dtsadminaddon/app/api/api2.php ''
-        url: 'http://localhost:80/dtsadminaddon/app/api/api2.php',
+        url: 'http://localhost:80/dtsadminaddon/api/api2.php',
         params: {
           woobar: new Date().getTime()
         },
@@ -34,7 +34,7 @@ angular.module('dtsAdminApp')
       return $http({
         method: 'GET',
         //TODO TODO TODO  switch onderstaande url: './api/ipa.php' --  'http://localhost:8888/dtsdts/app/api/ipa.php''
-        url: 'http://localhost:80/dtsadminaddon/app/api/ipa.php',
+        url: 'http://localhost:80/dtsadminaddon/api/ipa.php',
         params: {
           woobar: new Date().getTime()
         },
